a0966d93f7650e749aa5a5fe758b6ab806a24034
Alshidi494 <115731809+Alshidi494@users.noreply.github.com>
2025-07-07 23:37:40 +0400

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    alias(libs.plugins.android.application) version "8.6.0" apply false
+    alias(libs.plugins.jetbrains.kotlin.android) version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
