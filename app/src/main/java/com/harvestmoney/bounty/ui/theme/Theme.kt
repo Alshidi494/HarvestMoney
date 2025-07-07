7be1ea41ab24d61ec071ba5b872f2737aef5244d
Alshidi494 <115731809+Alshidi494@users.noreply.github.com>
2025-07-07 18:11:15 +0400

    Theme.kt

--- app/src/main/java/com/harvestmoney/bounty/ui/theme/Theme.kt
+++ app/src/main/java/com/harvestmoney/bounty/ui/theme/Theme.kt
@@ -1,42 +1,38 @@
 package com.harvestmoney.bounty.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimaryColor,
+    secondary = DarkSecondaryColor,
+    background = DarkBackgroundColor,
+    surface = DarkSurfaceColor,
+    onPrimary = DarkOnPrimaryColor,
+    onSecondary = DarkOnSecondaryColor,
+    onBackground = DarkOnBackgroundColor,
+    onSurface = DarkOnSurfaceColor,
+    error = ErrorColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    background = BackgroundColor,
+    surface = SurfaceColor,
+    onPrimary = OnPrimaryColor,
+    onSecondary = OnSecondaryColor,
+    onBackground = OnBackgroundColor,
+    onSurface = OnSurfaceColor,
+    error = ErrorColor
 )
 
 @Composable
 fun HarvestMoneyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -45,7 +41,6 @@ fun HarvestMoneyTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
