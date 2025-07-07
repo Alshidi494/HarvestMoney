20446b4c9528493a8aad6d8737ba1b0f3067a682
Alshidi494 <115731809+Alshidi494@users.noreply.github.com>
2025-07-07 23:51:20 +0400

    settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,17 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "com.android.application" -> useVersion("8.6.0")
+                "org.jetbrains.kotlin.android" -> useVersion("1.9.22")
+                "com.google.gms.google-services" -> useVersion("4.4.1")
+            }
+        }
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -21,4 +31,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Harvest Money"
 include(":app")
- 
